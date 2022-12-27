2e81810f70d14f8fc80c2af428c94085d2221345
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-12-27 16:35:04 +0900

    [Chore] 프로젝트 생성

---
+++ NotToDo/NotToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotToDo
+//
+//  Created by 강윤서 on 2022/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

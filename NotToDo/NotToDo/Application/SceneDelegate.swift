b280443dc58c7c658ec6d2aa6914d5dcf37eb6f1
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-12-27 17:00:27 +0900

    [Chore] #1 - 폴더링

---
+++ NotToDo/NotToDo/Application/SceneDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  SceneDelegate.swift
+//  NotToDo
+//
+//  Created by 강윤서 on 2022/12/27.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: scene)
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+
+
+}
+
---
+++ NotToDo/NotToDo/Global/Extensions/UIStackView+.swift
@@ -0,0 +1,8 @@
+//
+//  UIStackView+.swift
+//  NotToDo
+//
+//  Created by 강윤서 on 2022/12/27.
+//
+
+import Foundation

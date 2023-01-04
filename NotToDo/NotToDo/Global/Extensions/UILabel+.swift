a211450962fcba9143148d5f630194a499795fb9
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-01-04 11:55:16 +0900

    [ADD] #12 - UILabel Extension 추가

---
+++ NotToDo/NotToDo/Global/Extensions/UILabel+.swift
@@ -0,0 +1,33 @@
+//
+//  UILabel+.swift
+//  NotToDo
+//
+//  Created by 강윤서 on 2023/01/03.
+//
+
+import UIKit
+
+extension UILabel {
+    /// 라벨 일부 textColor 변경해주는 함수
+    /// - targetString에는 바꾸고자 하는 특정 문자열을 넣어주세요
+    /// - textColor에는 targetString에 적용하고자 하는 특정 UIColor에 넣어주세요
+    func partColorChange(targetString: String, textColor: UIColor) {
+        let fullText = self.text ?? ""
+        let range = (fullText as NSString).range(of: targetString)
+        let attributedString = NSMutableAttributedString(string: fullText)
+        attributedString.addAttribute(.foregroundColor, value: textColor, range: range)
+        self.attributedText = attributedString
+    }
+    
+    /// 라벨 일부 font 변경해주는 함수
+    /// - targerString에는 바꾸고자 하는 특정 문자열을 넣어주세요
+    /// - font에는 targetString에 적용하고자 하는 UIFont를 넣어주세요
+    func partFontChange(targetString: String, font: UIFont) {
+        let fullText = self.text ?? ""
+        let range = (fullText as NSString).range(of: targetString)
+        let attributedString = NSMutableAttributedString(string: fullText)
+        
+        attributedString.addAttribute(.font, value: font, range: range)
+        self.attributedText = attributedString
+    }
+}

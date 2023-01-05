d31f24a4c5a1c1a6f97305700549f19739b09d81
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-01-05 18:46:27 +0900

    [ADD] #17 - 내 정보 뷰 구현에 필요한 이미지 에셋과 파일 추가

---
+++ NotToDo/NotToDo/Presentation/MyInfoScene/Cells/myInfoFooterCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  myInfoFooterCollectionReusableView.swift
+//  NotToDo
+//
+//  Created by 강윤서 on 2023/01/05.
+//
+
+import UIKit
+
+class myInfoFooterCollectionReusableView: UICollectionReusableView {
+        
+}

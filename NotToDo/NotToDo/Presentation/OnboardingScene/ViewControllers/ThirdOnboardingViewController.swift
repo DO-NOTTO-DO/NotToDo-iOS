7eb9729621afff9796a02c7fd61d6c2f9d4cb683
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-01-10 03:18:43 +0900

    [DESIGN] #37 - thirdOnboarding 뷰 UI 구현

--- NotToDo/NotToDo/Presentation/OnboardingScene/ViewControllers/ThirdOnboardingViewController.swift
+++ NotToDo/NotToDo/Presentation/OnboardingScene/ViewControllers/ThirdOnboardingViewController.swift
@@ -7,23 +7,41 @@
 
 import UIKit
 
-class ThirdOnboardingViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class ThirdOnboardingViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private var onboardingView: ThirdOnboardingView!
+    
+    // MARK: - View Life Cycle
+    
+    override func loadView() {
+        super.loadView()
+        onboardingView = ThirdOnboardingView()
+        view = onboardingView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setAddTarget()
     }
-    */
+}
 
+extension ThirdOnboardingViewController {
+    private func setAddTarget() {
+        onboardingView.jumpButton.addTarget(self, action: #selector(buttonDidTap), for: .touchUpInside)
+        onboardingView.nextButton.addTarget(self, action: #selector(buttonDidTap), for: .touchUpInside)
+        onboardingView.beforeButton.addTarget(self, action: #selector(beforeButtonDidTap), for: .touchUpInside)
+    }
+    
+    // MARK: - @objc Methods
+    
+    @objc private func buttonDidTap() {
+        let nextViewController = FinishOnboardingViewController()
+        navigationController?.pushViewController(nextViewController, animated: true)
+    }
+    
+    @objc private func beforeButtonDidTap() {
+        navigationController?.popViewController(animated: true)
+    }
 }

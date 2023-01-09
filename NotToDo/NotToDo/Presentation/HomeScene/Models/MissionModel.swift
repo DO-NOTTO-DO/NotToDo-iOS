a717b5199953ea64eddbf49bb16a5f5c63cf85d1
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-01-09 18:10:59 +0900

    [ADD] #21 - 스크롤 영역 확인을 위한 더미 데이터 추가

--- NotToDo/NotToDo/Presentation/HomeScene/Models/MissionModel.swift
+++ NotToDo/NotToDo/Presentation/HomeScene/Models/MissionModel.swift
@@ -24,6 +24,17 @@ extension DailyMission {
                      completionStatus: "AMBIGUOUS", goal: "목 건강 지키기",
                      actions: [
                         "뜨거운 유자차 주문하기"
+                     ]),
+        DailyMission(title: "아요 사랑해", situation: "하루 종일",
+                     completionStatus: "AMBIGUOUS", goal: "아요를 사랑하자",
+                     actions: [
+                        "매일매일 김민서 보기",
+                        "매일매일 김정은 보기"
+                     ]),
+        DailyMission(title: "유튜브 보지 않기", situation: "기상 직후",
+                     completionStatus: "AMBIGUOUS", goal: "목 건강 지키기",
+                     actions: [
+                        "뜨거운 유자차 주문하기"
                      ])
     ]
     

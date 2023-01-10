4fb8fd123cef3058dc136cd7e67ce52dd3cfed37
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-01-10 15:27:23 +0900

    [ADD] #41 - 키보드 dismiss extension 추가

--- NotToDo/NotToDo/Presentation/AchievementScene/ViewControllers/SituationStatisticsViewController.swift
+++ NotToDo/NotToDo/Presentation/AchievementScene/ViewControllers/SituationStatisticsViewController.swift
@@ -1,16 +1,29 @@
 //
-//  SituationViewController.swift
+//  SituationStatisticsViewController.swift
 //  NotToDo
 //
-//  Created by JEONGEUN KIM on 2023/01/04.
+//  Created by 강윤서 on 2023/01/10.
 //
 
 import UIKit
 
 class SituationStatisticsViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // Do any additional setup after loading the view.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }

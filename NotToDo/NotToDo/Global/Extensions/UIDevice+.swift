4534b87569fb772f24efb7fa534c9dd88e50f9de
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-01-11 03:22:52 +0900

    [ADD] #44 - vibrate 생성 extension 추가

--- NotToDo/NotToDo/Global/Extensions/UIDevice+.swift
+++ NotToDo/NotToDo/Global/Extensions/UIDevice+.swift
@@ -5,4 +5,12 @@
 //  Created by 강윤서 on 2023/01/11.
 //
 
-import Foundation
+import UIKit
+
+import AVFoundation
+
+extension UIDevice {
+    static func vibrate() {
+        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+    }
+}

570b0555c46bcbab94e6d999faf743d195c999c7
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-01-10 18:29:44 +0900

    [ADD] #41 - IQKeyboardManager 라이브러리 설치

--- NotToDo/NotToDo/Application/AppDelegate.swift
+++ NotToDo/NotToDo/Application/AppDelegate.swift
@@ -7,10 +7,13 @@
 
 import UIKit
 
+import IQKeyboardManagerSwift
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        IQKeyboardManager.shared.enable = true
         return true
     }
     

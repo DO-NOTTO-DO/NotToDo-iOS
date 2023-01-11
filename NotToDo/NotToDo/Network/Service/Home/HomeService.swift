148e465cf7f79413c2f3f4d0643257937d984d35
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-01-12 02:36:14 +0900

    [ADD] #49 - 샘플 서비스 코드 추가

--- NotToDo/NotToDo/Network/Service/Home/HomeService.swift
+++ NotToDo/NotToDo/Network/Service/Home/HomeService.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+import Moya
+
+enum HomeService {
+    case banner
+}
+
+extension HomeService: TargetType {
+    var baseURL: URL {
+        return URL(string: URLConstant.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .banner:
+            return URLConstant.homeBanner
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .banner:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .banner:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .banner:
+            return NetworkConstant.hasTokenHeader
+        }
+    }
+}

c561fa74dceb06356468fe0e311579b9b1a74597
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-01-12 07:09:22 +0900

    [FEAT] #48 - 홈 뷰 actionSheet 플로우 구현

--- NotToDo/NotToDo/Presentation/HomeScene/ViewControllers/HomeViewController.swift
+++ NotToDo/NotToDo/Presentation/HomeScene/ViewControllers/HomeViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 import SnapKit
 import Then
 
-final class HomeViewController: UIViewController {
+final class HomeViewController: UIViewController, UICollectionViewDelegate {
+    
+    // MARK: - Properties
+    
+    var missionList: [DailyMission] = DailyMission.DailyMissionModel
     
     // MARK: - UI Components
     
@@ -27,10 +31,16 @@ final class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setAddTarget()
+        setDelegate()
     }
 }
 
 extension HomeViewController {
+    private func setDelegate() {
+        homeView.homeCollectionView.delegate = self
+        homeView.homeCollectionView.dataSource = self
+    }
+    
     private func setAddTarget() {
         homeView.addMissionButton.addTarget(self, action: #selector(addMission), for: .touchUpInside)
     }
@@ -41,5 +51,102 @@ extension HomeViewController {
         let addMissionViewController = AddMissionViewController()
         addMissionViewController.modalPresentationStyle = .overFullScreen
         present(addMissionViewController, animated: true)
+    }
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        switch indexPath.section {
+        case 0:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCalendarCollectionViewCell.identifier, for: indexPath) as? HomeCalendarCollectionViewCell else { return UICollectionViewCell() }
+            return cell
+        default:
+            if missionList.isEmpty {
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeEmptyCollectionViewCell.identifier, for: indexPath) as? HomeEmptyCollectionViewCell else { return UICollectionViewCell() }
+                return cell
+            } else {
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeMissionCollectionViewCell.identifier, for: indexPath) as? HomeMissionCollectionViewCell else { return UICollectionViewCell() }
+                cell.configure(missionList[indexPath.row])
+                cell.meatballClickedEvent = { result in
+                    if result {
+                        let actionSheetViewController = ActionSheetViewController()
+                        actionSheetViewController.modalPresentationStyle = .overFullScreen
+                        actionSheetViewController.modalTransitionStyle = .crossDissolve
+                        self.present(actionSheetViewController, animated: true)
+                        actionSheetViewController.dismissClicked = {
+                            // action sheet
+                            let calendarActionSheetViewController = ActionSheetViewController()
+                            calendarActionSheetViewController.mode = .calendar
+                            calendarActionSheetViewController.modalPresentationStyle = .overFullScreen
+                            calendarActionSheetViewController.modalTransitionStyle = .crossDissolve
+                            self.present(calendarActionSheetViewController, animated: true)
+                        }
+                    }
+                }
+                return cell
+            }
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        default:
+            return missionList.count
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionView.elementKindSectionHeader,
+              let header = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: HomeCollectionReusableView.identifier,
+                for: indexPath
+              ) as? HomeCollectionReusableView else { return UICollectionReusableView() }
+        header.setRandomData()
+        return header
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        switch section {
+        case 0:
+            return CGSize(width: Numbers.width, height: 183.adjusted)
+        default:
+            return CGSize()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellHeight = missionList[indexPath.row].actions.count < 2 ? 144.adjusted : 183.adjusted
+        switch indexPath.section {
+        case 0:
+            return CGSize(width: Numbers.width, height: 106.adjusted)
+        default:
+            if missionList.isEmpty {
+                return CGSize(width: Numbers.width, height: (collectionView.frame.height - 105).adjusted)            // 수정 필요
+            } else {
+                return CGSize(width: Numbers.width, height: cellHeight)
+            }
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        switch section {
+        case 0:
+            return UIEdgeInsets()
+        default:
+            return UIEdgeInsets(top: 7.adjusted, left: 0, bottom: 17.adjusted, right: 0)
+        }
     }
 }

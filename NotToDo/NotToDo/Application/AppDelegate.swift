203e5390526030ca524fc26d20a1bdeb5eaa7563
minseo205 <rlaalstj312@naver.com>
2023-02-07 14:22:58 +0900

    [FEAT] #104 - 카카오 로그인 초기 셋팅

--- NotToDo/NotToDo/Application/AppDelegate.swift
+++ NotToDo/NotToDo/Application/AppDelegate.swift
@@ -8,14 +8,30 @@
 import UIKit
 
 import IQKeyboardManagerSwift
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let nativeAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
+        
+        KakaoSDK.initSDK(appKey: nativeAppKey as! String)
+        
         IQKeyboardManager.shared.enable = true
+        
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+            if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                return AuthController.handleOpenUrl(url: url)
+            }
+
+            return false
+        }
     
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return UIInterfaceOrientationMask.portrait
